73f3d8f9ebf8d5a0fe91d3a199f4e80d835a460e
ClawmanCat <clawmancat@gmail.com>
2020-04-03 21:17:14 +0200

    disable default silence all

--- IMDBParser/IMDBParser.cpp
+++ IMDBParser/IMDBParser.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv) {
     auto v = std::vector<std::string>(argv, argv + argc);
-    v.push_back("--silence-all");
+    //v.push_back("--silence-all");
 
     IMDBParser::ParseController::instance().controller_main(IMDBParser::all_to_wstring(v));
 }
